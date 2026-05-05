236b89607596f6081378e76e4c9aacc3cf78066e
Patrick Callahan <patricktcallahan18@gmail.com>
2026-05-05 22:10:49 +0200

    Play around with creating get and post endpoints

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    id: int
+    name: str
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/custom/{item_id}", description="This is a custom function description")
+async def custom_func_name(item_id: str):
+    return {"message": f"This is a custom function name for item {item_id}"}
+
+
+@app.post("/items", description="Create an item")
+async def create_item(item: Item):
+    # write to a file in this project folder and create file if it doesn't exist
+    with open("items.txt", "a") as f:
+        f.write(f"{item.id}: {item.name}\n")
+    return {"message": f"Item created with id {item.id} and name {item.name}"}
