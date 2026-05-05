e4a6cbc2e464ed4534f6c263124d88d4a2ea4e1e
Patrick Callahan <patricktcallahan18@gmail.com>
2026-05-05 21:15:05 +0200

    Create hello world api

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from learning-fastapi!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
