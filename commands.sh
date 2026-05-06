5d161c946516eaa4002da02dd4d17408a0c1c7ec
Patrick Callahan <patricktcallahan18@gmail.com>
2026-05-06 15:20:11 +0200

    Add more useful commands

--- commands.sh
+++ commands.sh
@@ -1,3 +1,14 @@
 !#/bin/zsh
 
+# Install FastAPI and its dependencies
 uv add "fastapi[standard]"
+
+
+# Run the app
+fastapi dev
+
+# example posting to an endpoint from the command line
+# -H indicates the header, -d indicates the data to be sent in the request body
+curl -X POST "http://127.0.0.1:8000/items" \
+  -H "Content-Type: application/json" \
+  -d '{"id": 1, "name": "widget"}'
